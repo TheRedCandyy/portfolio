228575cf14a224b34f0175e049150dac355cadaa
Alexandre Tavares <prodalexandretavares@gmail.com>
2023-07-01 00:52:46 +0100

    update

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,6 @@
 import sadEmoji from './sademoji.webp'
+import splitterApp from './splitter_app.png'
+import crypto from './crypto.png'
 import { FaGithubSquare } from 'react-icons/fa'
 
 function Projects () {
@@ -12,10 +14,24 @@ function Projects () {
                         Working on it...
                     </p>
                     <p className="mt-4 max-w-2xl text-xl text-gray-500 xl:mx-auto">
-                       Since most of my projects have been to people that do not allow me to share any information, I can't add anything here... <img src={sadEmoji} alt="" className='w-5 inline-block'/>
+                       Since most of my projects have been to people that do not allow me to share any information, I can't add much here... <img src={sadEmoji} alt="" className='w-5 inline-block'/>
                     </p>
+                    <div className='mt-10 flex'>
+                        <div className='w-1/2 hover:scale-105 transition-all'>
+                            <a target="_blank" rel="noreferrer" href="https://theredcandyy.github.io/frontendmentor_solutions/tip-calculator-app-main/index.html">
+                                <center><img src={splitterApp} alt="Splitter App" className='h-72'/></center>
+                                <p className="mt-4 max-w-2xl text-md text-gray-500 xl:mx-auto">A Tip Calculator App</p>
+                            </a>
+                        </div>
+                        <div className='w-1/2 hover:scale-105 transition-all'>
+                            <a target="_blank" rel="noreferrer" href="https://theredcandyy.github.io/frontendmentor_solutions/nft-preview-card-component-main/index.html">
+                                <center><img src={crypto} alt="NFT Display Card" className='h-72 '/></center>
+                                <p className="mt-4 max-w-2xl text-md text-gray-500 xl:mx-auto">A NFT Display Card</p>
+                            </a>
+                        </div>
+                    </div>
                     <p className="mt-24 max-w-2xl text-md text-gray-500 xl:mx-auto">
-                        In the meantime, checkout my <a href='https://github.com/TheRedCandyy' className='text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-400 font-bold transition-all'>Github <button href='https://github.com/TheRedCandyy' className='bg-gradient-to-br from-purple-600 to-blue-400 rounded-md text-white hover:scale-110 transition-all'><FaGithubSquare/></button></a>, I might have added something new.
+                        Apart from this very small ideas you can checkout my <a href='https://github.com/TheRedCandyy' className='text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-400 font-bold transition-all'>Github <button href='https://github.com/TheRedCandyy' className='bg-gradient-to-br from-purple-600 to-blue-400 rounded-md text-white hover:scale-110 transition-all'><FaGithubSquare/></button></a>, I might have added something new.
                     </p>
                 </div>
             </div>
